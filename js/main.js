ae6f2be607f429b35df89001c5123eb55e1b6bce
Dustin-Davis <drpookydavis@comcast.net>
2022-01-07 10:46:11 -0800

    Fixed the capitals

--- js/main.js
+++ js/main.js
@@ -12,245 +12,245 @@ function fetchAPI(url) {
 }
 
 function getCreatures() {
-  var App = fetchAPI(creaturesURL);
-  var AppData = JSON.parse(App);
-  Array.from(AppData.data.non_food).sort((a, b) => a.id - b.id).forEach(item => {
+  var app = fetchAPI(creaturesURL);
+  var appData = JSON.parse(app);
+  Array.from(appData.data.non_food).sort((a, b) => a.id - b.id).forEach(item => {
 
-    var Container = document.createElement('div');
+    var container = document.createElement('div');
     var name = document.createElement('h2');
-    var IMG = document.createElement('img');
+    var img = document.createElement('img');
     // var description = document.createElement('p');
     // var commonLocation = document.createElement('ul');
-    // var Location = document.createElement('li');
-    // var Drops = document.createElement('ul');
+    // var location = document.createElement('li');
+    // var drop = document.createElement('ul');
     // var drops = document.createElement('li');
 
     name.innerText = item.name;
-    IMG.src = item.image;
+    img.src = item.image;
     // description.innerText = item.description;
     // commonLocation.innerText = 'Common Locations:';
-    // Location.innerText = `${item.common_locations}`;
-    // Drops.innerText = 'Drops:';
+    // location.innerText = `${item.common_locations}`;
+    // drop.innerText = 'Drops:';
     // drops.innerText = `${item.drops}`;
 
     // commonLocation.appendChild(Location);
-    // Drops.appendChild(drops);
-    Container.appendChild(IMG);
-    Container.appendChild(name);
-    // Container.appendChild(description);
-    // Container.appendChild(commonLocation);
-    // Container.appendChild(Drops);
+    // drop.appendChild(drops);
+    container.appendChild(img);
+    container.appendChild(name);
+    // container.appendChild(description);
+    // container.appendChild(commonLocation);
+    // container.appendChild(drop);
 
-    Container.classList.add('item');
+    container.classList.add('item');
     name.classList.add('name');
-    IMG.classList.add('image');
+    img.classList.add('image');
     // description.classList.add('description');
 
-    document.getElementsByClassName('creatures')[0].appendChild(Container);
+    document.getElementsByClassName('creatures')[0].appendChild(container);
   });
 }
 getCreatures();
 
 function getCreaturesFood() {
-  var App = fetchAPI(creaturesURL);
-  var AppData = JSON.parse(App);
-  Array.from(AppData.data.food).sort((a, b) => a.id - b.id).forEach(item => {
+  var app = fetchAPI(creaturesURL);
+  var appData = JSON.parse(app);
+  Array.from(appData.data.food).sort((a, b) => a.id - b.id).forEach(item => {
 
-    var Container = document.createElement('div');
+    var container = document.createElement('div');
     var name = document.createElement('h2');
-    var IMG = document.createElement('img');
+    var img = document.createElement('img');
     // var description = document.createElement('p');
     // var commonLocation = document.createElement('ul');
-    // var Location = document.createElement('li');
-    // var Drops = document.createElement('ul');
+    // var location = document.createElement('li');
+    // var drop = document.createElement('ul');
     // var drops = document.createElement('li');
 
     name.innerText = item.name;
-    IMG.src = item.image;
+    img.src = item.image;
     // description.innerText = item.description;
     // commonLocation.innerText = 'Common Locations:';
-    // Location.innerText = `${item.common_locations}`;
-    // Drops.innerText = 'Drops:';
+    // location.innerText = `${item.common_locations}`;
+    // drop.innerText = 'Drops:';
     // drops.innerText = `${item.drops}`;
 
     // commonLocation.appendChild(Location);
-    // Drops.appendChild(drops);
-    Container.appendChild(IMG);
-    Container.appendChild(name);
-    // Container.appendChild(description);
-    // Container.appendChild(commonLocation);
-    // Container.appendChild(Drops);
+    // drop.appendChild(drops);
+    container.appendChild(img);
+    container.appendChild(name);
+    // container.appendChild(description);
+    // container.appendChild(commonLocation);
+    // container.appendChild(drop);
 
-    Container.classList.add('item');
+    container.classList.add('item');
     name.classList.add('name');
-    IMG.classList.add('image');
+    img.classList.add('image');
     // description.classList.add('description');
 
-    document.getElementsByClassName('creatures')[0].appendChild(Container);
+    document.getElementsByClassName('creatures')[0].appendChild(container);
   });
 }
 getCreaturesFood();
 
 function getMonsters() {
-  var App = fetchAPI(monstersURL);
-  var AppData = JSON.parse(App);
-  Array.from(AppData.data).sort((a, b) => a.id - b.id).forEach(item => {
+  var app = fetchAPI(monstersURL);
+  var appData = JSON.parse(app);
+  Array.from(appData.data).sort((a, b) => a.id - b.id).forEach(item => {
 
-    var Container = document.createElement('div');
+    var container = document.createElement('div');
     var name = document.createElement('h2');
-    var IMG = document.createElement('img');
+    var img = document.createElement('img');
     // var description = document.createElement('p');
     // var commonLocation = document.createElement('ul');
-    // var Location = document.createElement('li');
-    // var Drops = document.createElement('ul');
+    // var location = document.createElement('li');
+    // var drop = document.createElement('ul');
     // var drops = document.createElement('li');
 
     name.innerText = item.name;
-    IMG.src = item.image;
+    img.src = item.image;
     // description.innerText = item.description;
     // commonLocation.innerText = 'Common Locations:';
-    // Location.innerText = `${item.common_locations}`;
-    // Drops.innerText = 'Drops:';
+    // location.innerText = `${item.common_locations}`;
+    // drop.innerText = 'Drops:';
     // drops.innerText = `${item.drops}`;
 
-    // commonLocation.appendChild(Location);
-    // Drops.appendChild(drops);
-    Container.appendChild(IMG);
-    Container.appendChild(name);
-    // Container.appendChild(description);
-    // Container.appendChild(commonLocation);
-    // Container.appendChild(Drops);
+    // commonLocation.appendChild(location);
+    // drop.appendChild(drops);
+    container.appendChild(img);
+    container.appendChild(name);
+    // container.appendChild(description);
+    // container.appendChild(commonLocation);
+    // container.appendChild(drop);
 
-    Container.classList.add('item');
+    container.classList.add('item');
     name.classList.add('name');
-    IMG.classList.add('image');
+    img.classList.add('image');
     // description.classList.add('description');
 
-    document.getElementsByClassName('monsters')[0].appendChild(Container);
+    document.getElementsByClassName('monsters')[0].appendChild(container);
   });
 }
 getMonsters();
 
 function getMaterials() {
-  var App = fetchAPI(materialsURL);
-  var AppData = JSON.parse(App);
-  Array.from(AppData.data).sort((a, b) => a.id - b.id).forEach(item => {
+  var app = fetchAPI(materialsURL);
+  var appData = JSON.parse(app);
+  Array.from(appData.data).sort((a, b) => a.id - b.id).forEach(item => {
 
-    var Container = document.createElement('div');
+    var container = document.createElement('div');
     var name = document.createElement('h2');
-    var IMG = document.createElement('img');
+    var img = document.createElement('img');
     // var description = document.createElement('p');
     // var commonLocation = document.createElement('ul');
-    // var Location = document.createElement('li');
-    // var CookingEffect = document.createElement('ul');
+    // var location = document.createElement('li');
+    // var cookingEffect = document.createElement('ul');
     // var effects = document.createElement('li');
-    // var HeartsRecovered = document.createElement('ul');
+    // var heartsRecovered = document.createElement('ul');
     // var recovered = document.createElement('li');
 
     name.innerText = item.name;
-    IMG.src = item.image;
+    img.src = item.image;
     // description.innerText = item.description;
     // commonLocation.innerText = 'Common Locations:';
-    // Location.innerText = `${item.common_locations}`;
-    // CookingEffect.innerText = 'Cooking Effects:';
+    // location.innerText = `${item.common_locations}`;
+    // cookingEffect.innerText = 'Cooking Effects:';
     // effects.innerText = `${item.cooking_effect}`;
-    // HeartsRecovered.innerText = 'Hearts Recovered:';
+    // heartsRecovered.innerText = 'Hearts Recovered:';
     // recovered.innerText = `${item.hearts_recovered}`;
 
     // commonLocation.appendChild(Location);
-    // CookingEffect.appendChild(effects);
-    // HeartsRecovered.appendChild(recovered);
-    Container.appendChild(IMG);
-    Container.appendChild(name);
-    // Container.appendChild(description);
-    // Container.appendChild(commonLocation);
-    // Container.appendChild(CookingEffect);
-    // Container.appendChild(HeartsRecovered);
+    // cookingEffect.appendChild(effects);
+    // heartsRecovered.appendChild(recovered);
+    container.appendChild(img);
+    container.appendChild(name);
+    // container.appendChild(description);
+    // container.appendChild(commonLocation);
+    // container.appendChild(cookingEffect);
+    // container.appendChild(heartsRecovered);
 
-    Container.classList.add('item');
+    container.classList.add('item');
     name.classList.add('name');
-    IMG.classList.add('image');
+    img.classList.add('image');
     // description.classList.add('description');
 
-    document.getElementsByClassName('materials')[0].appendChild(Container);
+    document.getElementsByClassName('materials')[0].appendChild(container);
   });
 }
 getMaterials();
 
 function getEquipment() {
-  var App = fetchAPI(equipmentURL);
-  var AppData = JSON.parse(App);
-  Array.from(AppData.data).sort((a, b) => a.id - b.id).forEach(item => {
+  var app = fetchAPI(equipmentURL);
+  var appData = JSON.parse(app);
+  Array.from(appData.data).sort((a, b) => a.id - b.id).forEach(item => {
 
-    var Container = document.createElement('div');
+    var container = document.createElement('div');
     var name = document.createElement('h2');
-    var IMG = document.createElement('img');
+    var img = document.createElement('img');
     // var description = document.createElement('p');
-    // var Stats = document.createElement('ul');
-    // var StatsAttack = document.createElement('li');
-    // var StatsDefense = document.createElement('li');
+    // var stats = document.createElement('ul');
+    // var statsattack = document.createElement('li');
+    // var statsDefense = document.createElement('li');
 
     name.innerText = item.name;
-    IMG.src = item.image;
+    img.src = item.image;
     // description.innerText = item.description;
-    // Stats.innerText = 'Stats';
-    // StatsAttack.innerText = `Attack: ${item.attack}`;
-    // StatsDefense.innerText = `Defense: ${item.defense}`;
+    // stats.innerText = 'Stats';
+    // statsattack.innerText = `attack: ${item.attack}`;
+    // statsDefense.innerText = `Defense: ${item.defense}`;
 
-    // Stats.appendChild(StatsAttack);
-    // Stats.appendChild(StatsDefense);
-    Container.appendChild(IMG);
-    Container.appendChild(name);
-    // Container.appendChild(description);
-    // Container.appendChild(Stats);
+    // stats.appendChild(statsattack);
+    // stats.appendChild(statsDefense);
+    container.appendChild(img);
+    container.appendChild(name);
+    // container.appendChild(description);
+    // container.appendChild(stats);
 
-    Container.classList.add('item');
+    container.classList.add('item');
     name.classList.add('name');
-    IMG.classList.add('image');
+    img.classList.add('image');
     // description.classList.add('description');
 
-    document.getElementsByClassName('equipment')[0].appendChild(Container);
+    document.getElementsByClassName('equipment')[0].appendChild(container);
   });
 }
 
 getEquipment();
 
 function getTreasure() {
-  var App = fetchAPI(treasureURL);
-  var AppData = JSON.parse(App);
-  Array.from(AppData.data).sort((a, b) => a.id - b.id).forEach(item => {
+  var app = fetchAPI(treasureURL);
+  var appData = JSON.parse(app);
+  Array.from(appData.data).sort((a, b) => a.id - b.id).forEach(item => {
 
-    var Container = document.createElement('div');
+    var container = document.createElement('div');
     var name = document.createElement('h2');
-    var IMG = document.createElement('img');
+    var img = document.createElement('img');
     // var description = document.createElement('p');
     // var commonLocation = document.createElement('ul');
-    // var Location = document.createElement('li');
-    // var Drops = document.createElement('ul');
+    // var location = document.createElement('li');
+    // var drop = document.createElement('ul');
     // var drops = document.createElement('li');
 
     name.innerText = item.name;
-    IMG.src = item.image;
+    img.src = item.image;
     // description.innerText = item.description;
     // commonLocation.innerText = 'Common Locations:';
-    // Location.innerText = `${item.common_locations}`;
-    // Drops.innerText = 'Drops:';
+    // location.innerText = `${item.common_locations}`;
+    // drop.innerText = 'Drops:';
     // drops.innerText = `${item.drops}`;
 
     // commonLocation.appendChild(Location);
-    // Drops.appendChild(drops);
-    Container.appendChild(IMG);
-    Container.appendChild(name);
-    // Container.appendChild(description);
-    // Container.appendChild(commonLocation);
-    // Container.appendChild(Drops);
+    // drop.appendChild(drops);
+    container.appendChild(img);
+    container.appendChild(name);
+    // container.appendChild(description);
+    // container.appendChild(commonLocation);
+    // container.appendChild(drop);
 
-    Container.classList.add('item');
+    container.classList.add('item');
     name.classList.add('name');
-    IMG.classList.add('image');
+    img.classList.add('image');
     // description.classList.add('description');
 
-    document.getElementsByClassName('treasure')[0].appendChild(Container);
+    document.getElementsByClassName('treasure')[0].appendChild(container);
   });
 }
 getTreasure();
