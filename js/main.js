70e2218e72e29e2a7bdedee9a19e65a7464e0394
Dustin-Davis <drpookydavis@comcast.net>
2022-11-17 17:11:49 -0800

    add a nav bar and user can switch between categorys

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ const fetchAPI = url => {
 };
 
 const creatures = document.getElementById('creatures');
+const mats = document.getElementById('mats');
+const cre = document.getElementById('cre');
+const flex = document.getElementById('flex-wrapper');
 
 function getCategories(category) {
   const app = fetchAPI('https://botw-compendium.herokuapp.com/api/v2/category' + '/' + category);
@@ -22,6 +25,7 @@ function getCategories(category) {
 
     if (index >= 6) return;
 
+    const Equipment = document.createElement('div');
     const Container = document.createElement('div');
     const Name = document.createElement('h2');
     const IMG = document.createElement('img');
@@ -44,23 +48,122 @@ function getCategories(category) {
     commonLocation.appendChild(Location);
     Stats.appendChild(StatsAttack);
     Stats.appendChild(StatsDefense);
+    Equipment.appendChild(Container);
     Container.appendChild(Name);
     Container.appendChild(IMG);
     Container.appendChild(Description);
     Container.appendChild(commonLocation);
     Container.appendChild(Stats);
 
+    Equipment.classList.add('equipment-div');
     Container.classList.add('item');
     Name.classList.add('name');
     IMG.classList.add('image');
     Description.classList.add('description');
 
-    document.getElementsByClassName('flex-wrapper')[0].appendChild(Container);
+    document.getElementsByClassName('flex-wrapper')[0].appendChild(Equipment);
   });
 }
 getCategories('equipment');
 
-creatures.addEventListener('click', () => { getCategories('creatures'); });
+function getCreature(category) {
+  const app = fetchAPI('https://botw-compendium.herokuapp.com/api/v2/category' + '/' + category);
+  const appData = JSON.parse(app);
+  let newArray = appData.data;
+
+  if (category === 'creatures') {
+    newArray = appData.data.food.concat(appData.data.non_food);
+  } else {
+    newArray = appData.data;
+  }
+
+  Array.from(newArray).sort((a, b) => a.id - b.id).forEach((item, index) => {
+
+    if (index >= 6) return;
+
+    const Container = document.createElement('div');
+    const Name = document.createElement('h2');
+    const IMG = document.createElement('img');
+    const Description = document.createElement('p');
+    const commonLocation = document.createElement('ul');
+    const Location = document.createElement('li');
+
+    Name.innerText = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+    IMG.src = item.image;
+    Description.innerHTML = item.description;
+    commonLocation.innerText = 'Common Locations:';
+    Location.innerText = `${item.common_locations}`.replaceAll(null, 'No Common Locations');
+
+    commonLocation.appendChild(Location);
+
+    Container.appendChild(Name);
+    Container.appendChild(IMG);
+    Container.appendChild(Description);
+    Container.appendChild(commonLocation);
+
+    Container.classList.add('item');
+    Name.classList.add('name');
+    IMG.classList.add('image');
+    Description.classList.add('description');
+
+    document.getElementsByClassName('creature')[0].appendChild(Container);
+  });
+}
+
+function getMaterials(category) {
+  const app = fetchAPI('https://botw-compendium.herokuapp.com/api/v2/category' + '/' + category);
+  const appData = JSON.parse(app);
+  let newArray = appData.data;
+
+  if (category === 'creatures') {
+    newArray = appData.data.food.concat(appData.data.non_food);
+  } else {
+    newArray = appData.data;
+  }
+
+  Array.from(newArray).sort((a, b) => a.id - b.id).forEach((item, index) => {
+
+    if (index >= 6) return;
+
+    const Container = document.createElement('div');
+    const Name = document.createElement('h2');
+    const IMG = document.createElement('img');
+    const Description = document.createElement('p');
+    const commonLocation = document.createElement('ul');
+    const Location = document.createElement('li');
+
+    Name.innerText = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+    IMG.src = item.image;
+    Description.innerHTML = item.description;
+    commonLocation.innerText = 'Common Locations:';
+    Location.innerText = `${item.common_locations}`.replaceAll(null, 'No Common Locations');
+
+    commonLocation.appendChild(Location);
+
+    Container.appendChild(Name);
+    Container.appendChild(IMG);
+    Container.appendChild(Description);
+    Container.appendChild(commonLocation);
+
+    Container.classList.add('item');
+    Name.classList.add('name');
+    IMG.classList.add('image');
+    Description.classList.add('description');
+
+    document.getElementsByClassName('mats')[0].appendChild(Container);
+  });
+}
+
+creatures.addEventListener('click', () => {
+  getCreature('creatures');
+  flex.classList.toggle('hidden');
+});
+
+mats.addEventListener('click', () => {
+  getMaterials('materials');
+  flex.classList.toggle('hidden');
+  cre.classList.toggle('hidden');
+});
 
 const scrollTop = document.querySelector('.scroll-top');
 
